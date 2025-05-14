f3e6970739d1bc6816ee07a9bf7bd3054c686cc4
Taj Sarkar <144534854+Taj-Sarkar@users.noreply.github.com>
2025-05-14 23:00:41 +0530

    Create NavBar.tsx

---
+++ NavBar.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  cartItemsCount: number;
+}
+const NavBar = ({ cartItemsCount }: Props) => {
+  return (
+    <div className="navbar bg-dark text-white p-3 mb-4 rounded">
+      <h4> 🛒 Cart Items: {cartItemsCount}</h4>
+    </div>
+  );
+};
+
+export default NavBar;
